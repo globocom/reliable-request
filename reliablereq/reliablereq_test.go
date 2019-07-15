b0d66c53c4596d6d6940da5c030b581a49a0a043
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2019-07-14 22:41:13 -0300

    add caching tests

--- reliablereq/reliablereq_test.go
+++ reliablereq/reliablereq_test.go
@@ -4,8 +4,6 @@ import (
 	"github.com/globocom/reliable-request/reliablereq"
 	"github.com/stretchr/testify/assert"
 	gock "gopkg.in/h2non/gock.v1"
-	"io/ioutil"
-	"net/http"
 	"testing"
 	"time"
 )
@@ -25,26 +23,24 @@ func Test_It_returns_a_valid_response(t *testing.T) {
 	gock.InterceptClient(req.HTTPClient)
 	defer gock.RestoreClient(req.HTTPClient)
 
-	resp, err := req.Request("http://example.com/list")
+	body, err := req.Get("http://example.com/list")
 
 	assert.Nil(t, err)
-	assert.NotNil(t, resp)
-	body, _ := ioutil.ReadAll(resp.Body)
-	assert.Equal(t, string(body), "{\"name\":\"mock\"}\n")
+	assert.NotNil(t, body)
+	assert.Equal(t, body, "{\"name\":\"mock\"}\n")
 }
 
-func Test_It_raises_error_when_there_is_no_connection(t *testing.T) {
+func Test_It_raises_an_error_when_there_is_no_connection(t *testing.T) {
 	reliablereq.Flush()
 
 	req := reliablereq.NewReliableRequest()
 
-	resp, err := req.Request("http://example.non/list")
+	_, err := req.Get("http://example.non/list")
 
-	assert.Nil(t, resp)
 	assert.NotNil(t, err)
 }
 
-func Test_It_returns_error_when_server_responds_with_a_non_2xx(t *testing.T) {
+func Test_It_returns_an_error_when_server_responds_with_a_non_2xx(t *testing.T) {
 
 	defer gock.Off()
 	reliablereq.Flush()
@@ -61,21 +57,20 @@ func Test_It_returns_error_when_server_responds_with_a_non_2xx(t *testing.T) {
 		gock.InterceptClient(req.HTTPClient)
 		defer gock.RestoreClient(req.HTTPClient)
 
-		resp, err := req.Request("http://example.com/list")
+		_, err := req.Get("http://example.com/list")
 
-		assert.Nil(t, resp)
 		assert.NotNil(t, err)
 	}
 }
 
 // ############### Caching ######################
-
 func Test_It_uses_cache_when_enabled(t *testing.T) {
 	defer gock.Off()
 	reliablereq.Flush()
 
 	gock.New("http://example.com").
 		Get("/list").
+		Times(1).
 		Reply(200).
 		JSON(map[string]interface{}{"name": "mock"})
 
@@ -85,26 +80,109 @@ func Test_It_uses_cache_when_enabled(t *testing.T) {
 	gock.InterceptClient(req.HTTPClient)
 	defer gock.RestoreClient(req.HTTPClient)
 
-	resp, err := req.Request("http://example.com/list")
+	body, err := req.Get("http://example.com/list")
 
 	assert.Nil(t, err)
-	assert.NotNil(t, resp)
-	body, _ := ioutil.ReadAll(resp.Body)
-	assert.Equal(t, string(body), "{\"name\":\"mock\"}\n")
+	assert.NotNil(t, body)
+	assert.Equal(t, body, "{\"name\":\"mock\"}\n")
 
-	resp, err = req.Request("http://example.com/list")
+	body, err = req.Get("http://example.com/list")
 
 	assert.Nil(t, err)
-	assert.NotNil(t, resp)
-	//https://medium.com/@xoen/golang-read-from-an-io-readwriter-without-loosing-its-content-2c6911805361
-	body, _ = ioutil.ReadAll(resp.Body)
-	assert.Equal(t, string(body), "{\"name\":\"mock\"}\n")
-
+	assert.NotNil(t, body)
+	assert.Equal(t, body, "{\"name\":\"mock\"}\n")
 }
-func Test_It_doesnt_use_cache_when_disable(t *testing.T) {}
 
-func Test_It_uses_stale_cache_when_enabled(t *testing.T)       {}
-func Test_It_doesnt_use_stale_cache_when_disable(t *testing.T) {}
+func Test_It_doesnt_use_cache_when_disabled(t *testing.T) {
+	defer gock.Off()
+	reliablereq.Flush()
+
+	gock.New("http://example.com").
+		Get("/list").
+		Times(1).
+		Reply(200).
+		JSON(map[string]interface{}{"name": "mock"})
+
+	req := reliablereq.NewReliableRequest()
+	req.EnableCache = false
+	// we need to intercept current http client due
+	// https://github.com/h2non/gock/issues/27#issuecomment-334177773
+	gock.InterceptClient(req.HTTPClient)
+	defer gock.RestoreClient(req.HTTPClient)
+
+	body, err := req.Get("http://example.com/list")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, body)
+	assert.Equal(t, body, "{\"name\":\"mock\"}\n")
+
+	body, err = req.Get("http://example.com/list")
+
+	assert.NotNil(t, err)
+}
+
+func Test_It_uses_stale_cache_when_enabled(t *testing.T) {
+	defer gock.Off()
+	reliablereq.Flush()
+
+	gock.New("http://example.com").
+		Get("/list").
+		Times(1).
+		Reply(200).
+		JSON(map[string]interface{}{"name": "mock"})
+
+	req := reliablereq.NewReliableRequest()
+	req.TTLCache = 1 * time.Second
+	// we need to intercept current http client due
+	// https://github.com/h2non/gock/issues/27#issuecomment-334177773
+	gock.InterceptClient(req.HTTPClient)
+	defer gock.RestoreClient(req.HTTPClient)
+
+	body, err := req.Get("http://example.com/list")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, body)
+	assert.Equal(t, body, "{\"name\":\"mock\"}\n")
+
+	// simulating a cache eviction
+	time.Sleep(2 * time.Second)
+
+	body, err = req.Get("http://example.com/list")
+
+	assert.Nil(t, err)
+}
+
+func Test_It_doesnt_use_stale_cache_when_disabled(t *testing.T) {
+	defer gock.Off()
+	reliablereq.Flush()
+
+	gock.New("http://example.com").
+		Get("/list").
+		Times(1).
+		Reply(200).
+		JSON(map[string]interface{}{"name": "mock"})
+
+	req := reliablereq.NewReliableRequest()
+	req.TTLCache = 1 * time.Second
+	req.EnableStaleCache = false
+	// we need to intercept current http client due
+	// https://github.com/h2non/gock/issues/27#issuecomment-334177773
+	gock.InterceptClient(req.HTTPClient)
+	defer gock.RestoreClient(req.HTTPClient)
+
+	body, err := req.Get("http://example.com/list")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, body)
+	assert.Equal(t, body, "{\"name\":\"mock\"}\n")
+
+	// simulating a cache eviction
+	time.Sleep(2 * time.Second)
+
+	body, err = req.Get("http://example.com/list")
+
+	assert.NotNil(t, err)
+}
 
 // ############### Caching ######################
 
@@ -116,6 +194,7 @@ func Test_It_doesnt_use_stale_cache_when_disable(t *testing.T) {}
 // 2. caching (enabled/disabled)
 // 3. stale caching (enabled/disabled)
 // 4. custom headers
+// 5. all hystrix features (circuit break on and off)
 
 // unhappy path
 // 1. non 2xx (should raise err)
@@ -124,4 +203,6 @@ func Test_It_doesnt_use_stale_cache_when_disable(t *testing.T) {}
 // future
 // [ ] retry policy
 // [ ] async hystrix (Go instead of Do)
+// [ ] understand and test the simultaneous client req hystrix config to see its implications
 // [ ] load stress
+// [ ] other reliable methods PUT POST
